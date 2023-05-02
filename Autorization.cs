b03b4f77eef7aff03d0c887dd38c5e6652f93e51
clody8 <saksonov8@mail.ru>
2023-05-02 11:06:14 +0300

    Начатая форма курсового проекта.

---
+++ Autorization.cs
@@ -0,0 +1,15 @@
+namespace ООП_Курсач___Код
+{
+    public partial class Autorization : Form
+    {
+        public Autorization()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Menu.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ООП_Курсач___Код
+{
+    partial class Database
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(213, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Меню";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(152, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 64);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Поиск по музыканту";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(152, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(231, 64);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Вывод данных";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Database
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(534, 518);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Database";
+            this.Text = "Database";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+    }
+}
